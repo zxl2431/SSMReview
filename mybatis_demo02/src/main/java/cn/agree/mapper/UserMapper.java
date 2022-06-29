16e4f1357fa271b8b770c4b640db1870c6efbc49
zhangxianlin <zhangxianlin2431@163.com>
2022-06-29 23:26:16 +0800

    MyBatis实现增删改查

--- mybatis_demo02/src/main/java/cn/agree/mapper/UserMapper.java
+++ mybatis_demo02/src/main/java/cn/agree/mapper/UserMapper.java
@@ -4,7 +4,34 @@ import cn.agree.domain.User;
 
 import java.util.List;
 
+
 public interface UserMapper {
+    /*
+     *  查询所有
+     *
+     * */
     List<User> findAll();
+
+    /*
+     *  添加用户
+     *
+     * */
+    int saveUser(User user);
+
+    /*
+    *  修改用户
+    * */
+    int updateUser(User user);
+
+    /*
+    *  删除用户
+    * */
+    int deleteUser(Integer id);
+
+
 }
+
+
+
+
 
--- mybatis_demo02/src/test/java/cn/agree/MyBatisTest.java
+++ mybatis_demo02/src/test/java/cn/agree/MyBatisTest.java
@@ -6,10 +6,12 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.Date;
 import java.util.List;
 
 public class MyBatisTest {
@@ -26,7 +28,7 @@ public class MyBatisTest {
             InputStream is = Resources.getResourceAsStream("SqlMapConfig.xml");
             SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
             SqlSessionFactory sqlSessionFactory = builder.build(is);
-            SqlSession sqlSession = sqlSessionFactory.openSession();
+            sqlSession = sqlSessionFactory.openSession();
             userMapper = sqlSession.getMapper(UserMapper.class);
         } catch (Exception e) {
             e.printStackTrace();
@@ -41,5 +43,46 @@ public class MyBatisTest {
         }
     }
 
+    @Test
+    public void testSaveUser() {
+        User user = new User();
+        user.setUsername("黎明");
+        user.setSex("男");
+        user.setBirthday(new Date());
+        user.setAddress("香港");
+        int i = userMapper.saveUser(user);
+        System.out.println("新增User的id:"+user.getId());
+
+    }
+
+    /*
+    *  修改用户
+    * */
+    @Test
+    public void testUpdateUser() {
+        User user = new User();
+        user.setUsername("郭富城");
+        user.setSex("男");
+        user.setBirthday(new Date());
+        user.setAddress("香港");
+        user.setId(74);
+        userMapper.updateUser(user);
+        // System.out.println("新增User的id:"+user.getId());
+    }
+
+    /*
+    *  删除用户
+    * */
+    @Test
+    public void testDeleteUser() {
+        userMapper.deleteUser(2);
+    }
+
+
+    @After
+    public void destory() {
+        sqlSession.commit();
+        sqlSession.close();
+    }
 
 }
