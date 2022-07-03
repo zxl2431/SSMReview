58c9f866f0750b5e4ccc6986d330caf29a14eedc
zhangxianlin <zhangxianlin2431@163.com>
2022-07-03 22:38:10 +0800

    Spring和Junit的整合

--- spring_demo02/src/main/java/cn/agree/web/Client03.java
+++ spring_demo02/src/main/java/cn/agree/web/Client03.java
@@ -1,4 +1,22 @@
 package cn.agree.web;
 
+import cn.agree.service.AccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:bean.xml"})
 public class Client03 {
+
+    @Autowired
+    private AccountService as;
+
+    @Test
+    public void TestSave() {
+        as.save();
+    }
+
 }
