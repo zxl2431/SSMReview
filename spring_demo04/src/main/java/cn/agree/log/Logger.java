64573a6c94e12fa942bb477c61157b7d230a6bc1
zhangxianlin <zhangxianlin2431@163.com>
2022-07-04 22:24:33 +0800

    Spring 前置通知,后置通知,异常通知,最终通知的xml配置

--- spring_demo04/src/main/java/cn/agree/log/Logger.java
+++ spring_demo04/src/main/java/cn/agree/log/Logger.java
@@ -4,9 +4,36 @@ public class Logger {
 
     /*
     *
+    *  前置通知
     *  用于打印日志
     * */
     public void printLog() {
-        System.out.println("Logger类中printLog方法开始记录日志了...");
+        System.out.println("Logger类中printLog方法开始记录日志了before...");
     }
+
+    /*
+    * 后置通知
+    * */
+    public void afterLog() {
+        System.out.println("--------afterReturning-------------------");
+    }
+
+    /***
+     * after-throwing
+     * 异常通知
+     */
+    public void error(){
+        System.out.println("--------after-throwing--------");
+    }
+
+    /***
+     * after
+     * 最终通知
+     */
+    public void after(){
+        System.out.println("---------after----------");
+    }
+
+
+
 }
