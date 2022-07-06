4ff7e57a620baa93328ab4760ada8bcd814aa0d1
zhangxianlin <zhangxianlin2431@163.com>
2022-07-06 23:55:31 +0800

    @RequestMapping方法中 入参参数总结: 1、提交表单的name和方法中入参名称是一模一样的 2、如果接收JavaBean则必须和JavaBean的属性相同

---
+++ springmvc_demo01/src/main/java/cn/agree/controller/UserController.java
@@ -0,0 +1,58 @@
+package cn.agree.controller;
+
+import cn.agree.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+
+    /*
+    *  接受基本类型和String
+    *  注意url请求参数的名字要和函数入参的名字一致
+    * */
+    // http://localhost:8080/user/add1?id=78&name=%E6%97%B6%E4%BB%A3%E5%B3%BB%E5%B3%B0%E8%82%AF%E5%AE%9A%E6%98%AF
+    @RequestMapping(value = "/add1")
+    public String addUser(int id, String name) {
+        System.out.println("id:"+id+",name:"+name);
+        return "success";
+    }
+
+    /*
+    * 接受参数是javabean类型
+    *
+    * */
+    @RequestMapping(value = "/add2")
+    public String addUser(User user) {
+        System.out.println("age:"+user.getAge()+",name:"+user.getName());
+        return "success";
+    }
+
+    /*
+    *
+    * 接受Map类型的参数
+    * */
+    @RequestMapping(value = "/add3")
+    public String addUser(@RequestParam Map userMap) {
+        System.out.println(userMap);
+        return "success";
+    }
+
+    /*
+    * 接受List参数类型
+    *
+    * */
+    @RequestMapping(value = "/add4")
+    public String addUser(@RequestParam List<Integer> id) {
+        System.out.println(id);
+        return "success";
+    }
+
+
+
+}
